58068ca45a2b4307cbb38e924ce6b10117646b20
XBinary <wbn1883@vip.qq.com>
2023-06-29 10:12:53 +0800

    update

---
+++ UniFree/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using zFramework.IO;
+
+static void ExecuteCommand(string command)
+{
+    var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+    processInfo.CreateNoWindow = true;
+    processInfo.UseShellExecute = false;
+    processInfo.RedirectStandardError = true;
+    processInfo.RedirectStandardOutput = true;
+
+    var process = Process.Start(processInfo);
+    if (process != null) {
+        process.OutputDataReceived += (_, e) => Console.WriteLine(e.Data);
+        process.BeginOutputReadLine();
+
+        process.ErrorDataReceived += (_, e) => Console.WriteLine(e.Data);
+        process.BeginErrorReadLine();
+
+        process.WaitForExit();
+
+        //Console.WriteLine("ExitCode: {0}", process.ExitCode);
+        process.Close();
+    }
+}
+
+try {
+    Console.WriteLine("UniFree v1.0 by binaryinject");
+    Console.WriteLine("==========================选择文件==========================");
+    var file = FileDialog.SelectFile("选择Unity.exe", "Unity.exe|Unity.exe");
+
+    if (file.Count == 0) {
+        Console.WriteLine("未选择Unity.exe!");
+        Console.ReadKey();
+        return;
+    }
+
+    Console.WriteLine($"选择Unity路径：{file[0]}");
+
+    Console.WriteLine("==========================开始补丁==========================");
+
+    var basePath = file[0][.. (file[0].Length - "Unity.exe".Length)];
+    var dllPath = $@"{basePath}Data\Resources\Licensing\Client\Unity.Licensing.EntitlementResolver.dll";
+
+    if (!File.Exists(dllPath)) {
+        Console.WriteLine("Unity.Licensing.EntitlementResolver.dll未找到！");
+        Console.ReadKey();
+        return;
+    }
+
+    ExecuteCommand($"patch-windows-amd64.exe \"{dllPath}\"");
+
+    Console.WriteLine("==========================开始签名==========================");
+
+    File.Delete("Unity_lic.ulf");
+    ExecuteCommand("sign-windows-amd64.exe");
+
+    if (!File.Exists("Unity_lic.ulf")) {
+        Console.WriteLine("签名失败！");
+    }
+
+    Console.WriteLine("==========================授权拷贝==========================");
+    File.Copy("Unity_lic.ulf", @"C:\ProgramData\Unity\Unity_lic.ulf", true);
+
+    Console.WriteLine("破解完成！");
+    Console.ReadKey();
+}
+catch (Exception e) {
+    Console.WriteLine(e);
+    throw;
+}
